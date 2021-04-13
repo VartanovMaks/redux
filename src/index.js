05076e0249c68e984f78be932ed8519b41601916
VartanovMaks <vartanov2009@gmail.com>
2021-04-13 12:07:52 +0300

    Сделали редюсер

--- src/index.js
+++ src/index.js
@@ -3,6 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+const initialState = {
+  counter:0
+}
+const reducer = (state = initialState, action)=>{
+  console.log('reducer', action);
+  switch (action.type){
+    case 'INC':{
+      return {
+        ...state,
+        counter: state.counter+1
+      }
+    }
+    case 'DEC':{
+      return {
+      ...state,
+      counter: state.counter-1
+      }
+    }
+    case 'RESET':{
+      return {
+        ...state,
+        counter: 0
+        }
+    }
+    default :
+    return state;
+  }
+}
+const store=createStore(reducer)
+
+console.log(store);
+store.subscribe(()=>{
+  console.log('subscriber');
+})
+
+store.dispatch({type:'INC'})
+console.log(store.getState());
+store.dispatch({type:'INC'})
+console.log(store.getState());
+store.dispatch({type:'INC'})
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>

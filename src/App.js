e57edfb18e6450903d6368246824ceeb9fd6cf50
VartanovMaks <vartanov2009@gmail.com>
2021-04-21 13:54:41 +0300

    Разнесли по компонентам

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* {!(counter1 %2) && <PhotosList />} */}
       <Products />
-      {/* <h1>
-        {`Counter 1 : ${counter1}`}
-      </h1>
-      <h1>
-        {`Counter 2 : ${counter2}`}
-      </h1>
-      <button onClick={()=>dispatch(incCustomAction(102))}>inc custom</button>
-      <button onClick={()=>dispatch(incAction())}>increment</button>
-      <button onClick={()=>dispatch(decAction())}>decrement</button>
-      <button onClick={()=>dispatch(reset())}>reset</button> */}
     </div>
   );
 }
